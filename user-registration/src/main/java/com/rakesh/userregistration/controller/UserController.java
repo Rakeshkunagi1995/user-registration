47a2cc3a723ced5406691a9a6d33f95b1dbd9e80
rakesh <rakeshkunagi95@gmail.com>
2024-02-17 19:37:30 +0530

    Initial commit: Added new files

---
+++ user-registration/src/main/java/com/rakesh/userregistration/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.rakesh.userregistration.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.rakesh.userregistration.model.User;
+import com.rakesh.userregistration.repository.UserRepository;
+
+
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/registration")
+	public String getRegPage(@ModelAttribute("user") User user) {
+		return "register";
+	}
+	
+	@PostMapping("/registration")
+	public String saveUser(@ModelAttribute("user") User user, Model model) {
+		userRepository.save(user);
+		model.addAttribute("message", "Submitted Successfully");
+		return "register";
+	}
+	
+	@GetMapping("/users")
+	public String usersPage(Model model) {
+		List<User> listOfUsers = userRepository.findAll();
+		model.addAttribute("user", listOfUsers);
+		return "user";
+	}
+
+}
---
+++ user-registration/src/test/java/com/rakesh/userregistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.userregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
